326b6cf47c8b09b38d482f643e9c3a2b2b1f085c
Diego Jimenez <115049957+DiegoJimenez14@users.noreply.github.com>
2026-02-03 12:31:55 -0500

    Initialize project structure and base files
    Updated project structure and created base files.

--- setup_project.py
+++ setup_project.py
@@ -1,9 +1,9 @@
 import os
 
-# Nombre del proyecto principal
+
 project_name = "dibulla-hotel-analytics"
 
-# Estructura de carpetas
+
 folders = [
     f"{project_name}/data/raw",
     f"{project_name}/data/processed",
@@ -14,25 +14,24 @@ folders = [
     f"{project_name}/outputs/reports"
 ]
 
-# Crear carpetas
+
 for folder in folders:
     os.makedirs(folder, exist_ok=True)
     print(f" Carpeta creada: {folder}")
 
-# Crear archivos base
-# 1. README.md
+
 with open(f"{project_name}/README.md", "w", encoding='utf-8') as f:
     f.write(f"# Proyecto de Inteligencia de Datos: {project_name}\n\n")
     f.write("Estrategia de reactivación turística basada en datos abiertos (Open Source).\n")
 
-# 2. .gitignore (Para no subir el archivo de 400MB a GitHub por error)
+
 with open(f"{project_name}/.gitignore", "w", encoding='utf-8') as f:
     f.write("data/raw/*\n")        # Ignora los datos crudos pesados
     f.write("*.DS_Store\n")
     f.write("__pycache__/\n")
     f.write(".env\n")
 
-# 3. requirements.txt
+
 with open(f"{project_name}/requirements.txt", "w", encoding='utf-8') as f:
     f.write("pandas\nrequests\nmatplotlib\nopenmeteo-requests\n")
 
